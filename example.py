3571620328e9fe53e507d2fc1155e580d32a7522
nurih <1554735+nurih@users.noreply.github.com>
2020-12-14 17:15:20 -0800

    srv uri support

--- example.py
+++ example.py
@@ -6,6 +6,10 @@ from gen_spec import IntGenSpec
 generator.with_field('score', IntGenSpec(0, 10))
 
 # Run
-doc_count = generator.run(cli_args.iteration_count)
+try:
+    doc_count = generator.run(cli_args.iteration_count)    
+    
+    print(f'{doc_count} documents found')
+except Exception as e:
+    print(e)
 
-print(f'{doc_count} found')
